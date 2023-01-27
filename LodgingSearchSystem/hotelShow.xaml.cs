a0325aefa6924bfe2f6b2aafdcd46e94823056c8
33002西島彩 <33002@ojs.ac.jp>
2023-01-27 16:00:31 +0900

    0127

--- LodgingSearchSystem/hotelShow.xaml.cs
+++ LodgingSearchSystem/hotelShow.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
 
 namespace LodgingSearchSystem
 {
@@ -71,51 +73,76 @@ namespace LodgingSearchSystem
             //住所
             var address1 = json1.hotels[0].hotel[0].hotelBasicInfo.address1;
             var address2 = json1.hotels[0].hotel[0].hotelBasicInfo.address2;
-            //アクセス
-            var access = json1.hotels[0].hotel[0].hotelBasicInfo.access;
+           
+            
             //最低代金
             var mincharge = json1.hotels[0].hotel[0].hotelBasicInfo.hotelMinCharge;
+
             //ホテルの写真
-            var imageurl = json1.hotels[0].hotel[0].hotelBasicInfo.hotelImageUrl;
-            BitmapImage imagesourse = new BitmapImage(new Uri(imageurl));
-            imHotel.Source = imagesourse;
-            
+            Image[] images = { imHotel1, imHotel2, imHotel3, imHotel4, imHotel5, imHotel6, imHotel7, imHotel8, imHotel9, imHotel10 };
+            for (int i = 0; i < images.Length; i++)
+            {
+                var imageurl = json1.hotels[i].hotel[0].hotelBasicInfo.hotelImageUrl;
+                BitmapImage imagesourse = new BitmapImage(new Uri(imageurl));
+                images[i].Source = imagesourse;
+            }
+
+
+            //アクセス
+            Label[] labelAccessArray = { lbaccess1, lbaccess2, lbaccess3, lbaccess4, lbaccess5, lbaccess6, lbaccess7, lbaccess8, lbaccess9, lbaccess10 };
+            for(int i = 0; i < labelAccessArray.Length; i++)
+            {
+                labelAccessArray[i].Content = json1.hotels[i].hotel[0].hotelBasicInfo.access;
+            }
+
+            //ホテルの名前
+            Label[] hotelName = { lbHotelName1, lbHotelName2, lbHotelName3, lbHotelName4, lbHotelName5, lbHotelName6, lbHotelName7, lbHotelName8, lbHotelName9, lbHotelName10 };
+            for (int i = 0; i < hotelName.Length; i++)
+            {
+                hotelName[i].Content = json1.hotels[i].hotel[0].hotelBasicInfo.hotelName;
+            }
+
+            //string text = access;
+            //foreach (string item in Regex.Matches(text,@".{1,15}").Cast<Match>().Select(m => m.Value).ToArray())
+            //{
+            //    lbaccess1.Content = access1;
+            //}
 
             lbPrefName.Content = area+"　ホテル・旅館";
             lbRecordCount.Content = String.Format("{0}件中", recordcount);
             lbDisplay.Content = String.Format("{0}～{1}表示", displayfirst, displaylast);
-            lbHotelName.Content = hotelname;
-            lbServiceAverage.Content = serviceaverage;
-            lbHotelSpecial.Content = hotelspecial;
-            lbPostalCode.Content = String.Format("〒{0}",postalcode);
-            lbaddress.Content = String.Format("{0}{1}", address1, address2);
-            lbaccess.Content = access;
-            lbMinCharge.Content = String.Format("{0:N0}円～", mincharge);
+            lbHotelName1.Content = hotelname;
+            lbServiceAverage1.Content = serviceaverage;
+            lbHotelSpecial1.Content = hotelspecial;
+            lbPostalCode1.Content = String.Format("〒{0}",postalcode);
+            lbaddress1.Content = String.Format("{0}{1}", address1, address2);
+            //lbaccess.Content = access;
+            lbMinCharge1.Content = String.Format("{0:N0}円～", mincharge);
 
 
-            if (serviceaverage >= 5){
-                ;
-            }else if(serviceaverage >= 3.8){
-                im5.Source = null;
-            }else if(serviceaverage >= 2.8){
-                im5.Source = null;
-                im4.Source = null;
-            }else if(serviceaverage >= 1.8){
-                im5.Source = null;
-                im4.Source = null;
-                im3.Source = null;
-            }else if(serviceaverage >= 1){
-                im5.Source = null;
-                im4.Source = null;
-                im3.Source = null;
-                im2.Source = null;
-            }else{
-                im5.Source = null;
-                im4.Source = null;
-                im3.Source = null;
-                im2.Source = null;
-                im1.Source = null;
-            }
+            //if (serviceaverage >= 5){
+            //    ;
+            //}else if(serviceaverage >= 3.8){
+            //    im5.Source = null;
+            //}else if(serviceaverage >= 2.8){
+            //    im5.Source = null;
+            //    im4.Source = null;
+            //}else if(serviceaverage >= 1.8){
+            //    im5.Source = null;
+            //    im4.Source = null;
+            //    im3.Source = null;
+            //}else if(serviceaverage >= 1){
+            //    im5.Source = null;
+            //    im4.Source = null;
+            //    im3.Source = null;
+            //    im2.Source = null;
+            //}else{
+            //    im5.Source = null;
+            //    im4.Source = null;
+            //    im3.Source = null;
+            //    im2.Source = null;
+            //    im1.Source = null;
+            //}
         }
 
         private void btMap_Click(object sender, RoutedEventArgs e)
@@ -125,11 +152,6 @@ namespace LodgingSearchSystem
         }
 
         private void btRecommend_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btAreaSelect_Click(object sender, RoutedEventArgs e)
         {
 
         }
